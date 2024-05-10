ca79ed4bd490e5a4a768e00d51d52e356aee5a2e
Dhauni777 <neerajdhauni777@gmail.com>
2024-05-11 01:14:52 +0530

    First Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,5 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Lottery from './Lottery'
-import TicketNum from './TicketNum'
-import Ticket from './Ticket'
 import { sum } from './helper'
 
 function App() {
