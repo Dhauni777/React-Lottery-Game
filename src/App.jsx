e9358348b1feb91ce548bfb3f6db356a5eb670bc
Dhauni777 <neerajdhauni777@gmail.com>
2024-05-11 01:06:30 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Lottery from './Lottery'
+import TicketNum from './TicketNum'
+import Ticket from './Ticket'
+import { sum } from './helper'
+
+function App() {
+
+  let winCondition = (ticket)=>{
+    return sum(ticket)===15;
+  }
+
+  return (
+    <>
+      
+      <Lottery n={3} winCondition={winCondition}/>
+    </>
+  );
+}   
+
+export default App
